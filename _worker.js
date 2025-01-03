68d6b7740e031e36fb712e7a4d4b481b36d8dea1
M3h64 <134771675+M3h64@users.noreply.github.com>
2025-01-04 00:43:27 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '73ed240d-57b6-4fda-8458-a9b40cf5653d';
+let userID = '975dc309-90ad-4312-b0e6-437beffc8743';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
